6b8697e3ccc7d1bdc65211161eae4a70bba79ddd
teliva <telitsyna.ivan@gmail.com>
2026-04-03 18:13:27 -0400

    Updated the caching for fetched data from the database

--- CatalogDataTransformer/Program.cs
+++ CatalogDataTransformer/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 
 try
 {
+
     var config = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
@@ -11,9 +12,28 @@ try
     string connectionString = config.GetConnectionString("KITSProduct")
     ?? throw new ArgumentException("No connection string", nameof(connectionString));
 
+    var relativePath = config["DataCacheLocation"];
+
+    if (string.IsNullOrWhiteSpace(relativePath))
+    {
+        throw new InvalidOperationException("Configuration 'DataCacheLocation' is missing or empty.");
+    }
+
+    var options = new JsonSerializerOptions
+    {
+        WriteIndented = true
+    };
+
     MainController mc = new MainController(connectionString);
-    // var ans = await mc.GetCatalogNodes(1479000001); // Sample KFI catalog, this ID is liable to change
-    var ans = await mc.GetCatalogProducts(1479000001);
+    var ans = await mc.GetCatalogProducts(186000001);
+
+    string fileName = "CatalogProducts_186000001.json";
+    string filePath = Path.Combine(relativePath, fileName);
+
+    string jsonString = JsonSerializer.Serialize(ans, options);
+    await File.WriteAllTextAsync(filePath, jsonString);
+
+    Console.WriteLine($"Data successfully written to: {filePath}");
 
     Environment.ExitCode = (int)ExitCodes.Success;
 }
