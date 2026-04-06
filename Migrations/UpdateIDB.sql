a91f75917760b9b0450770ad47d9389457d4462c
teliva <telitsyna.ivan@gmail.com>
2026-04-06 18:59:36 -0400

    Updated migrations

--- Migrations/UpdateIDB.sql
+++
@@ -1,8 +0,0 @@
--- Create new table to store embeddings that are generated for an IDB.
-CREATE TABLE ProductEmbeddings (
-    ProductId INT FOREIGN KEY NOT NULL,
-    Product_DescriptionEmbeddings VARCHAR(MAX)
-
-    CONSTRAINT FK_Embeddings_Products FOREIGN KEY (ProductId) 
-    REFERENCES Product(ProductId)
-);
---
+++ Migrations/create_product_table.sql
@@ -0,0 +1,8 @@
+-- Create the Product Table inside postgres
+CREATE TABLE Product (
+    productId INT PRIMARY KEY,
+    modelNumber VARCHAR(50),
+    pceProduct BOOLEAN,
+    description TEXT,
+    enhancedDescription TEXT
+);
